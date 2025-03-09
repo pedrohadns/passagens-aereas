222324d7eee4d2df2ef22f46b5e29429ff6d246b
Silvio Eduardo Bellinazzi de Andrade <162626372+Siltx@users.noreply.github.com>
2025-03-09 14:02:56 -0300

    Update assentos_disponíveis.c

--- assentos_disponíveis.c
+++ assentos_disponíveis.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define COR_VERDE "\x1b[32m"   
 #define COR_VERMELHO "\x1b[31m" 
 #define RESET "\x1b[0m" 
-
-#define LINHAS 10
-#define COLUNAS 10
-#define ARQUIVO_ASSENTOS "assentos.dat"
+#define FILEIRAS 25
+#define COLUNAS 6
+#define ARQUIVO_ASSENTOS "assentos.txt"
+#define ARQUIVO_VOOS "voos.dat"
 
 typedef struct {
     char assento[4];  // Nome do assento (ex: "10A")
@@ -16,108 +17,108 @@ typedef struct {
 } Assento;
 
 typedef struct {
-    Assento n_assentos[LINHAS][COLUNAS]; 
+    Assento n_assentos[FILEIRAS][COLUNAS]; 
 } Matriz;
 
-// salvar os assentos no arquivo
-void salvar_assentos(Matriz *m) {
-    FILE *file = fopen(ARQUIVO_ASSENTOS, "wb");
-    if (file == NULL) {
-        printf("Erro ao salvar os assentos.\n");
-        return;
-    }
-    fwrite(m, sizeof(Matriz), 1, file);
-    fclose(file);
-}
+typedef struct {
+    int id;
+    char assento[4];
+} Voo;
 
-// carregar os assentos do arquivo
 void carregar_assentos(Matriz *m) {
-    FILE *file = fopen(ARQUIVO_ASSENTOS, "rb");
+    FILE *file = fopen(ARQUIVO_ASSENTOS, "r");
     if (file != NULL) {
-        fread(m, sizeof(Matriz), 1, file);
+        for (int i = 0; i < FILEIRAS; i++) {
+            for (int j = 0; j < COLUNAS; j++) {
+                fscanf(file, "%s %d", m->n_assentos[i][j].assento, &m->n_assentos[i][j].disponivel);
+            }
+        }
         fclose(file);
     } else {
-        // Se o arquivo não existir, inicializa a matriz com assentos disponíveis
-        for (int i = 0; i < LINHAS; i++) {
+        for (int i = 0; i < FILEIRAS; i++) {
             for (int j = 0; j < COLUNAS; j++) {
                 snprintf(m->n_assentos[i][j].assento, 4, "%d%c", i + 1, 'A' + j);
                 m->n_assentos[i][j].disponivel = 1;
             }
         }
-        salvar_assentos(m);  
     }
 }
 
 void exibir_assentos_disponiveis(Matriz *m) {
-    printf("\n Visualização dos Assentos:\n");
-    for (int i = 0; i < LINHAS; i++) {
+    printf("\n Assentos:\n");
+    for (int i = 0; i < FILEIRAS; i++) {
         for (int j = 0; j < COLUNAS; j++) {
             if (m->n_assentos[i][j].disponivel == 1) {
                 printf(COR_VERDE "[%s] " RESET, m->n_assentos[i][j].assento);
             } else {
                 printf(COR_VERMELHO "[X] " RESET);
             }
+            if (j == 2) printf("   "); // Espaço entre grupos de poltronas para o corredor
         }
         printf("\n");
     }
 }
 
-void escolha_de_assento(Matriz *m) {
+void escolha_de_assento(Matriz *m, int id) {
     char escolha[4];
-    exibir_assentos_disponiveis(m); 
+    exibir_assentos_disponiveis(m);
 
-    printf("\nEscolha um assento: ");
+    printf("\nPor favor, escolha um assento: ");
     scanf("%s", escolha);
 
-    // Procurar o assento na matriz
-    for (int i = 0; i < LINHAS; i++) {
+    for (int i = 0; i < FILEIRAS; i++) {
         for (int j = 0; j < COLUNAS; j++) {
             if (strcmp(m->n_assentos[i][j].assento, escolha) == 0) {
                 if (m->n_assentos[i][j].disponivel == 1) {
                     m->n_assentos[i][j].disponivel = 0;
-                    printf("assento %s escolhido com sucesso! tenha uma boa viagem\n", escolha);
-                    salvar_assentos(m);  // Atualiza o arquivo com a nova reserva
+                    printf("O assento %s escolhido com sucesso!\n", escolha);
                     return;
                 } else {
-                    printf("infelizmente o assento %s já está ocupado! Por favor, escolha outro.\n", escolha);
-                    escolha_de_assento(m); 
+                    printf("O assento %s já está ocupado!\n", escolha);
                     return;
                 }
             }
         }
     }
-    printf("escorregou ai? Código de assento inválido!\n");
-    escolha_de_assento(m);
+    printf("Assento invalido!\n");
 }
 
+void consultar_voo_e_exibir_assentos(Matriz *m) {
+    int id;
+    printf("Digite o ID do voo para visualizar os assentos: ");
+    scanf("%d", &id);
+    exibir_assentos_disponiveis(m);
+}
 
 int main() {
+    srand(time(NULL));
     Matriz m;
-    carregar_assentos(&m); 
+    carregar_assentos(&m);
 
-    int opcao;
+    int opcao, id;
     do {
-        printf("\n==== SELEÇÃO DE ASSENTOS ====\n");
-        printf("[1] Mostrar assentos\n");
-        printf("[2] Reservar assento\n");
-        printf("[0] Sair\n");
-        printf("o'que deseja? ");
+        printf("\n==== MENU ===="
+               "\n[1] Visualizar Assentos de um Voo"
+               "\n[2] Reservar Assento"
+               "\n[0] Sair"
+               "\n O que deseja? ");
         scanf("%d", &opcao);
-
+        getchar();
         switch (opcao) {
             case 1:
-                exibir_assentos_disponiveis(&m);
+                consultar_voo_e_exibir_assentos(&m);
                 break;
             case 2:
-                escolha_de_assento(&m);
+                printf("Digite o ID do voo: ");
+                scanf("%d", &id);
+                escolha_de_assento(&m, id);
                 break;
             case 0:
-                printf("ação encerrada\n");
+                printf("Obrigado! Volte sempre!\n");
                 break;
             default:
-                printf("Opção inválida! Tente novamente.\n");
+                printf("Opção inválida!\n");            
+                break;
         }
     } while (opcao != 0);
-
-    return 0;
 }
